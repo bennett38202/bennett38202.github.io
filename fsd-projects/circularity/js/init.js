77a52a4b950866868366e4af59e8c26129084b31
bennett38202 <bennett38202@students.apsb.org>
2025-06-04 16:08:50 +0000

    circularity with no crosses in the circles

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -25,7 +25,7 @@ var init = function (window) {
 
         // TODO 2 : Create a function that draws a circle 
         function drawCircle() {
-            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            circle = draw.randomCircleInArea(canvas, true, false, "#999", 2);
             physikz.addRandomVelocity(circle, canvas, 5, 5);
             view.addChild(circle);
             circles.push(circle);
